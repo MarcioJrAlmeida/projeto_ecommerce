73c611aa20d262985c2d1003c843018562b55a89
TatianaBeltrao <tatiana.beltrao.ac@hotmail.com>
2025-10-29 13:07:03 -0300

    FEAT: Add instructions to run in CMD

---
+++ project-ecommerce/frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+// RUN THIS COMMAND IN TERMINAL BASH
+
+// # from the repo root (or wherever you want)
+// mkdir frontend && cd frontend
+// npm create vite@latest . -- --template react-ts
+// npm i axios @tanstack/react-query react-router-dom zustand
+// # optional styling
+// npm i -D tailwindcss postcss autoprefixer
+// npx tailwindcss init -p 
