52cdb54a0ea2d6014d534fa4b32e8d44d5ebc0dd
marcio <marciojr.almeida96@gmail.com>
2025-12-11 10:10:35 -0300

    Adição da Variavel de Ambiente

--- project-ecommerce/dist/data-source.js
+++ project-ecommerce/dist/data-source.js
@@ -3,13 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppDataSource = void 0;
 require("reflect-metadata");
 const typeorm_1 = require("typeorm");
+require("dotenv/config");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "mysql",
     host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "root@123",
-    database: "ecommerce",
+    port: Number(process.env.MYSQL_PORT),
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     synchronize: true,
     logging: false,
     entities: ["src/entity/*.ts"],
